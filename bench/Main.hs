81dbce090882ab058a8efc04778b81bda91911ad
Michael Gale <m.gale@warwick.ac.uk>
2019-02-08 12:32:55 +0000

    Fix benchmark name to avoid JS issues

--- bench/Main.hs
+++ bench/Main.hs
@@ -11,14 +11,14 @@ import Lab5
 
 main :: IO ()
 main = defaultMain [
-    bgroup "foos (inefficient)" [
+    bgroup "foos (should be exponential)" [
         bench "10" $ nf (take 10 . foos 1) 2,
         bench "20" $ nf (take 20 . foos 1) 2,
         bench "30" $ nf (take 30 . foos 1) 2{-,
         bench "35" $ nf (take 35 . foos 1) 2,
         bench "40" $ nf (take 40 . foos 1) 2-}
     ],
-    bgroup "foos' (efficient)" [
+    bgroup "foosPrime (should be linear)" [
         bench "10" $ nf (take 10 . foos' 1) 2,
         bench "20" $ nf (take 20 . foos' 1) 2,
         bench "30" $ nf (take 30 . foos' 1) 2{-,
